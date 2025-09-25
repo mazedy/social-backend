2bbb10c0b9c53e929957111b26aa6e9de10b4a2c
mazedy <mazeedab18@example.com>
2025-09-25 15:35:35 +0700

    Initial commit - social media backend

--- app/config.py
+++ app/config.py
@@ -5,4 +5,8 @@ from neomodel import config
 load_dotenv()
 
 NEO4J_BOLT_URL = os.getenv("NEO4J_BOLT_URL")
-config.DATABASE_URL = NEO4J_BOLT_URL
+NEO4J_USER = os.getenv("NEO4J_USER")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+
+# Correct Aura connection string
+config.DATABASE_URL = f"{NEO4J_BOLT_URL}://{NEO4J_USER}:{NEO4J_PASSWORD}@e0791f27.databases.neo4j.io"
